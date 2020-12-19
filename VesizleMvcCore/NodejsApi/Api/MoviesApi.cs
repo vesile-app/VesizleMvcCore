dd1831cb3df357c3262f50ff6df942751fed688e
Ali <aliyildizoz909@gmail.com>
2020-12-19 18:42:22 +0300

    Update

--- VesizleMvcCore/NodejsApi/Api/MoviesApi.cs
+++ VesizleMvcCore/NodejsApi/Api/MoviesApi.cs
@@ -34,19 +34,19 @@ namespace VesizleMvcCore.NodejsApi.Api
 
             return new ApiPopularResult();
         }
-        public async Task<ApiMovieDetailsResult> GetDiscoverAsync()
+        public async Task<ApiDiscoverResult> GetDiscoverAsync()
         {
-            var url = ApiPath + id.ToString();
+            var url = ApiPath + "discovery";
             var res = await _httpClient.GetAsync(url);
 
             if (res.IsSuccessStatusCode)
             {
                 var result = await res.Content.ReadAsStringAsync();
-                var jsonDeserialize = JsonHelper.Deserialize<ApiMovieDetailsResult>(result);
+                var jsonDeserialize = JsonHelper.Deserialize<ApiDiscoverResult>(result);
                 return jsonDeserialize;
             }
 
-            return new ApiMovieDetailsResult();
+            return new ApiDiscoverResult();
         }
         public async Task<ApiMovieDetailsResult> GetDetailsAsync(int id)
         {
