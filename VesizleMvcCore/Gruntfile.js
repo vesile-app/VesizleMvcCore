1b06a3b2e5eee45231c4afa507b87f4ad0bd2d16
Ali <aliyildizoz909@gmail.com>
2020-12-19 10:06:57 +0300

    Update

---
+++ VesizleMvcCore/Gruntfile.js
@@ -0,0 +1,51 @@
+﻿/// <binding ProjectOpened='watch' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ["wwwroot/lib/*", "temp/"],
+        concat: {
+            dist: {
+                src: ['wwwroot/js/*.js'],
+                dest: 'temp/combined.js'
+            }
+        },
+        jshint: {
+            files: ['temp/*.js'],
+            options: {
+                '-W069': false,
+            }
+        },
+        uglify: {
+            all: {
+                src: ['temp/combined.js'],
+                dest: 'wwwroot/lib/combined.min.js'
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['wwwroot/js/*.js'],
+                tasks: ['all'],
+                options: {
+                    spawn: false
+                }
+            },
+            css: {
+                files: ['wwwroot/css/*.css'],
+                tasks: ['all']
+            }
+        },
+        cssmin: {
+            target: {
+                src: ['wwwroot/css/*.css', 'wwwroot/css/!*.min.css'],
+                dest: 'wwwroot/lib/vesile.min.css'
+            }
+        }
+    });
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.registerTask("all", ['clean', 'concat', 'jshint', 'uglify', 'cssmin']);
+
+};
--- VesizleMvcCore/Startup.cs
+++ VesizleMvcCore/Startup.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using VesizleMvcCore.Constants;
 using VesizleMvcCore.NodejsApi.Api;
@@ -52,7 +55,12 @@ namespace VesizleMvcCore
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), @"node_modules")),
+                RequestPath = new PathString("/node_modules")
+            });
             app.UseRouting();
 
             app.UseAuthorization();
